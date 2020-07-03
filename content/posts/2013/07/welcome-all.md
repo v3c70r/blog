f0c08ac755755bb8e263833bb3e940d983dd9d6e
Tsing Gu <mcvector.gu@gmail.com>
2020-07-03 18:29:15 -0400

    Revert "Generate Pelican site"
    This reverts commit 840d18d6d3b7d6b2204cb87befca7a0eb66ace5c.

---
+++ content/posts/2013/07/welcome-all.md
@@ -0,0 +1,10 @@
+Title: Hello World
+Slug: HelloWorld
+Date: 2020-06-21 02:00
+Lang: en
+Category: Life
+Tags: Hello
+Author: Qing Gu
+Summary: A simple test post
+
+This is a test post to test the migration of blog system to Pelican.
---
+++ pelicanconf.py
@@ -0,0 +1,38 @@
+from pathlib import Path
+# Theme Settings
+THEME = 'themes/aboutwilson'
+## used for OG tags and Twitter Card data on index page
+#SITEIMAGE = 'site-cover.jpg'
+## used for OG tags and Twitter Card data of index page
+
+###################################
+OUTPUT_PATH = 'output'
+DEFAULT_LANG = 'en'
+ARTICLE_URL = 'posts/{date:%Y}/{date:%m}/{slug}/'
+ARTICLE_SAVE_AS = 'posts/{date:%Y}/{date:%m}/{slug}/index.html'
+ARTICLE_LANG_URL = 'posts/{date:%Y}/{date:%m}/{slug}-{lang}/'
+ARTICLE_LANG_SAVE_AS = 'posts/{date:%Y}/{date:%m}/{slug}-{lang}/index.html'
+PATH = 'content'
+
+# Feed generation is usually not desired when developing
+FEED_ALL_ATOM = None
+CATEGORY_FEED_ATOM = None
+TRANSLATION_FEED_ATOM = None
+AUTHOR_FEED_ATOM = None
+AUTHOR_FEED_RSS = None
+
+# Custom Home page
+AUTHOR = 'Qing Gu'
+SITENAME = "QGU"
+SITESUBTITLE = 'A personal blog.'
+SITEURL = 'http://qgu.io/blog'
+ABSOLUTE_URL = SITEURL
+TIMEZONE = "America/Montreal"
+LOCALE = ("en_CA", "fr_CA", "zh_CN")
+HOME = str(Path.home())
+DEFAULT_PAGINATION = 4
+DEFAULT_DATE = (2012, 3, 2, 14, 1, 1)
+RELATIVE_URLS = True
+
+DEFAULT_PAGINATION = False
+
